c2cd74949810019b694259452194c10e98aa2997
binod tiwari <bnodtwari111@gmail.com>
2021-04-30 19:42:11 +0545

    hidden secret key????

--- BMIcalculator/settings.py
+++ BMIcalculator/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rfhd&9w)v#b44-h4)km55h1!@8#^t&c&jl0gpz85i9mz!1bazf'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
